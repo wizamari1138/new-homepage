9d9e31ed2c03462474403ea268ecf92d23237765
wizamari1138 <g03106@ktc.ac.jp>
2026-06-28 16:11:23 +0900

    SG改行に変更

--- src/components/certificationbox/page.tsx
+++ src/components/certificationbox/page.tsx
@@ -3,13 +3,21 @@ import { BsPassport, BsPcDisplayHorizontal } from "react-icons/bs";
 import { MdOutlineMedicalInformation, MdSecurity } from "react-icons/md";
 
 type IconType = "Passport" | "Information" | "PcDisplay" | "MdSecurity";
+type NameType = "ITパスポート" | "情報技術検定1級" | "基本情報技術者試験" | "情報セキュリティマネジメント試験";
 
 type Props = {
-    name: string;
+    name: NameType;
     description: string;
     date: string;
     iconName: IconType;
 }
+
+const nameMap = {
+    "ITパスポート": <>ITパスポート</>,
+    "情報技術検定1級": <>情報技術検定1級</>,
+    "基本情報技術者試験": <>基本情報技術者試験</>,
+    "情報セキュリティマネジメント試験": <>情報セキュリティ<br />マネジメント試験</>
+};
 
 const iconMap = {
     Passport: <BsPassport className={Style.icon} />,
@@ -23,7 +31,7 @@ export default function CertificationBox({name, description, date, iconName}: Pr
   return (
     <div className={Style.certificationBox}>
       {icon}
-      <p className={Style.name}>{name}</p>
+      <p className={Style.name}>{nameMap[name]}</p>
       <p className={Style.description}>
         発行元：{description} <br/>
         取得年月日：{date}
